6c5ab5d5aa6057a753c0c40915a46dd2cc93b367
sudo-Whateverman <nick@smartgreen.co.il>
2017-11-10 16:27:10 +0200

    spellcheck

--- smash.c
+++ smash.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
     char cmdString[MAX_LINE_SIZE]; 	   
 
 	
-	//signal declaretions
+	//signal declarations
 	//NOTE: the signal handlers and the function/s that sets the handler should be found in siganls.c
 	 /* add your code here */
 	
